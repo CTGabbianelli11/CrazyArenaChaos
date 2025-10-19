1500ed126d7fd7fbe0786bebdd65b7aa6004a9e2
Christian Gabbianelli <108819775+CTGabbianelli11@users.noreply.github.com>
2025-10-19 18:55:03 -0400

    Update Currency.cpp

--- Source/CrazyArenaChaos/Private/Items/Currency.cpp
+++ Source/CrazyArenaChaos/Private/Items/Currency.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Characters/CPPCharacter.h"
 #include "Items/Currency.h"
+#include "Characters/CPPCharacter.h"
+
 
 void ACurrency::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
