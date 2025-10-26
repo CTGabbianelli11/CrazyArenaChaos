51a71a69c86fcbff4164e7b3d561ff231b17817e
Christian Gabbianelli <108819775+CTGabbianelli11@users.noreply.github.com>
2025-10-25 22:25:16 -0400

    Update Currency.h

--- Source/CrazyArenaChaos/Public/Items/Currency.h
+++ Source/CrazyArenaChaos/Public/Items/Currency.h
@@ -16,5 +16,7 @@ class CRAZYARENACHAOS_API ACurrency : public AItem
 protected:
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 
+	UPROPERTY(EditAnywhere, Category = "Currency Poperties")
+	int32 currencyValue;
 	
 };
