4d45a274e57bf64675d8c2624cc30b6e313a683b
Christian Gabbianelli <108819775+CTGabbianelli11@users.noreply.github.com>
2025-11-12 19:17:19 -0500

    Created WeaponDataAsset to hold information about eachweapon

---
+++ Source/CrazyArenaChaos/Public/Items/Weapons/WeaponDataAsset.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "WeaponDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(Blueprintable, BlueprintType)
+class CRAZYARENACHAOS_API UWeaponDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	bool HasBeenBought=false;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool IsEquipped=false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Price;
+	
+};
