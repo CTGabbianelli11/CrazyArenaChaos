0062fbb5204e3db39f88bdfacd81e826b8fadaf8
Christian Gabbianelli <108819775+CTGabbianelli11@users.noreply.github.com>
2025-12-03 23:58:44 -0500

    Moved logic to c++ script to have better control

--- Source/CrazyArenaChaos/Private/UI/ListItem.cpp
+++ Source/CrazyArenaChaos/Private/UI/ListItem.cpp
@@ -3,3 +3,14 @@
 
 #include "UI/ListItem.h"
 
+void UListItem::NativeOnListItemObjectSet(UObject* ListItemObject)
+{
+	IUserObjectListEntry::NativeOnListItemObjectSet(ListItemObject);
+
+	if (ListItemObject&&Cast<UWeaponDataAsset>(ListItemObject))
+	{
+		weaponDataAsset = Cast<UWeaponDataAsset>(ListItemObject);
+
+		GEngine->AddOnScreenDebugMessage(1, 1.f, FColor::Red, weaponDataAsset->WeaponName);
+	}
+}
--- Source/CrazyArenaChaos/Public/UI/ListItem.h
+++ Source/CrazyArenaChaos/Public/UI/ListItem.h
@@ -4,14 +4,31 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Blueprint/IUserObjectListEntry.h"
+#include "Items/Weapons/WeaponDataAsset.h"
+#include "Components/Button.h"
 #include "ListItem.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class CRAZYARENACHAOS_API UListItem : public UUserWidget
+class CRAZYARENACHAOS_API UListItem : public UUserWidget,public IUserObjectListEntry
 {
+
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(BlueprintReadOnly,meta = (BindWidget))
+	UButton* EquipButton; // Name this exactly as your button in UMG
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UButton* BuyButton; // Name this exactly as your button in UMG
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
+	UButton* UpgradeButton; // Name this exactly as your button in UMG
+
+	UPROPERTY(BlueprintReadWrite)
+	UWeaponDataAsset* weaponDataAsset;
+
+public:
+	virtual void NativeOnListItemObjectSet(UObject* ListItemObject) override;
 };
