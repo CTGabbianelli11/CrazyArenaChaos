401542099cb66b2a631443d1a81945ebbde94458
Cody McCarty <codyjmccarty@gmail.com>
2026-01-21 21:04:48 -0600

    Currency pickup fx.

--- Source/CrazyArenaChaos/Private/Items/Currency.cpp
+++ Source/CrazyArenaChaos/Private/Items/Currency.cpp
@@ -1,5 +1,7 @@
 
 #include "Items/Currency.h"
+
+#include "Components/SphereComponent.h"
 #include "Interfaces/PickupInterface.h"
 
 void ACurrency::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
@@ -9,6 +11,11 @@ void ACurrency::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor
     if (OtherActor && OtherActor->GetClass()->ImplementsInterface(UPickupInterface::StaticClass()))
     {
         IPickupInterface::Execute_AddCurrency(OtherActor, this);
-        Destroy();
+        
+        //~ we don't want to pick it up twice, but we can't destroy it until fx is played.
+        SetActorEnableCollision(false);
+        sphereCollider->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+        
+        BP_CurrencyCollected(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
     }
 }
--- Source/CrazyArenaChaos/Public/Items/Currency.h
+++ Source/CrazyArenaChaos/Public/Items/Currency.h
@@ -1,4 +1,3 @@
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -8,15 +7,21 @@
 UCLASS()
 class CRAZYARENACHAOS_API ACurrency : public AItem
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
 protected:
-    virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 
-    UPROPERTY(EditAnywhere, Category = "Currency Properties")
-    int32 currencyValue;
+	/** NOTE (cody) I noticed there's an int32 currencyValue in the cpp *AND* BP_Currency has an int64 CurrencyAmount. I'm not 100% but these may be intended for the same thing. If not, delete this comment. */
+	UPROPERTY(EditAnywhere, Category = "Currency Properties")
+	int32 currencyValue;
+
+	/** Use this function to play any fx when currency is collected. Remember to destroy the actor afterward.  */
+	UFUNCTION(BlueprintImplementableEvent, meta=(DisplayName = "Currency Collected"))
+	void BP_CurrencyCollected(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 public:
-    FORCEINLINE int32 GetCurrency() { return currencyValue; }
+	FORCEINLINE int32 GetCurrency() { return currencyValue; }
 };
